f45c885ba28cb342e54ce94e483a10fccf13bb32
olaoluwapaul <adesolapaul@gmail.com>
2023-10-06 17:13:14 +0200

    End of Section 1👨‍💻

--- DatingApp/Entities/AppUser.cs
+++ DatingApp/Entities/AppUser.cs
@@ -1,10 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+//uses using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.Entities
 {
     public class AppUser
     {
-        //[Key] uses using System.ComponentModel.DataAnnotations;
+        //[Key] 
         public int Id { get; set; }
         public string UserName { get; set; }
     }
